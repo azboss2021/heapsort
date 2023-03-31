f705ccb2f1656aafe0c3105e2d5d42d37e1eb909
azboss2021 <alexanderzyn1@gmail.com>
2023-03-30 20:20:09 -0700

    Setup sidebar and initial states

--- heapsort/src/App.jsx
+++ heapsort/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [grid, setGrid] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setGrid([[0]]);
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="flex">
+      <section className="h-screen p-2 w-64 overflow-y-scroll">
+        <label htmlFor="insert-node">Node {1}</label>
+        <input
+          id="insert-node border-2"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Node Value"
+        ></input>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Insert Node
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      </section>
+      <section className="">
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex}>
+            {row.map((node, nodeIndex) => (
+              <div
+                key={nodeIndex}
+                className="w-20 h-20 bg-blue-500 border-2 rounded-full"
+              ></div>
+            ))}
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
